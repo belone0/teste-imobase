ee4b4171fee432489859bff4fb625a6f9645cd0a
beloner <belone00@gmail.com>
2023-09-10 21:46:11 -0300

    redirect

--- app/Http/Controllers/ImovelController.php
+++ app/Http/Controllers/ImovelController.php
@@ -75,7 +75,7 @@ class ImovelController extends Controller
             return redirect()->route('imoveis.index');
         }catch (\Throwable $th){
             Log::error($th->getMessage());
-            return redirect()->route('imoveis.index');
+            return redirect()->back();
         }
     }
 
